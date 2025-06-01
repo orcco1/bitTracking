255a2fd3c8e171478c0d5dbc54b16a4b7b83f967
Adrian Alejandro Cruz Alanis <alejandrocruzalanis@gmail.com>
2025-06-01 00:39:32 -0600

    Track BTC

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import time
 from binance.client import Client
 
 with open("config.json") as f:
@@ -6,5 +7,22 @@ with open("config.json") as f:
 
 client = Client(config["api_key"], config["api_secret"])
 
-price = client.get_symbol_ticker(symbol="BTCUSDT")
-print(f"Precio actual BTC/USDT: {price['price']}")
+# Tracking
+symbol = "BTCUSDT"
+alert_threshold = 60000.0
+
+print(f"Tracking {symbol}... Alert if dips under ${alert_threshold}")
+
+while True:
+    try:
+        ticker = client.get_symbol_ticker(symbol=symbol)
+        price = float(ticker['price'])
+        print(f"[{time.strftime('%H:%M:%S')}] Actual prince: ${price:.2f}")
+
+        if price < alert_threshold:
+            print(f"🚨 Alert: {symbol} dips under ${alert_threshold} → actual price: ${price:.2f}")
+
+        time.sleep(30)
+    except Exception as e:
+        print(f"❌ Error: {e}")
+        time.sleep(10)
